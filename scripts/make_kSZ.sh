4a759e3403adc75cbb88ae5a1f62709247fad5c6
elizabeth mc bride <emc-brid@cluster-head.ias.u-psud.fr>
2025-01-09 14:47:12 +0100

    added slurm script

---
+++ scripts/make_kSZ.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=run_kSZ
+#SBATCH --time=02-00:00:00
+#SBATCH --output=output.log
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=lisaleemcb@gmail.com
+#SBATCH --ntasks-per-node=12
+
+# use the bash shell
+set -x
+# echo each command to standard out before running it
+date
+
+# source bash profile
+source /home/emc-brid/.bashrc
+
+# run the Unix 'date' command
+echo "Hello world, from the Cluster!"
+# run the Unix 'echo' command
+which mamba
+mamba activate kSZ
+which python
+python -u /home/emc-brid/kSZ/scripts/make_kSZ.py 
